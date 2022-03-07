88b8dd8ee531b5b73e75896aff7be7596b8d2ab5
matutosi <matutosi@gmail.com>
2022-03-07 11:37:42 +0900

    etc

--- R/search_similar_name.R
+++ R/search_similar_name.R
@@ -1,39 +1,39 @@
   #' Search similar name(s) from existing data
-  #' 
-  #' search_similar_name() for one string. 
+  #'
+  #' search_similar_name() for one string.
   #' maybe() for multiple scientific names.
   #' mosiya() for multiple wamei (Japanese names).
-  #' 
+  #'
   # '@name search_similar_name
-  #' 
-  #' @param x A String or a vector of strings to be checked. 
+  #'
+  #' @param x A String or a vector of strings to be checked.
   #' @param len integer 1: when checking scientific name, 6: when checking wamei (Japanese name).
   #' @param min_dist Minimum editing distance. Less than min_dist will be output.
   #' @param min_dist_norm Minimum normalised editing distance.
-  #' 
-  #' @return Tibble. 
-  #' 
+  #'
+  #' @return Tibble.
+  #'
   #' @seealso maybe(), mosiya()
-  #' 
+  #'
   #' @examples
   #' library(stringi)
   #' library(tidyverse)
   #' x <- "Viola madahuricaa"
   #' search_similar_name(x, len=1)
-  #' 
-  #' x <- c("\\u30cf\\u30c3\\u30ab\\u30b0\\u30b5") %>% 
+  #'
+  #' x <- c("\\u30cf\\u30c3\\u30ab\\u30b0\\u30b5") %>%
   #'   stringi::stri_unescape_unicode()
   #' search_similar_name(x, len=6)
-  #' 
+  #'
   #' x <- c("Viola madahuricaa", "Carex nevarta")
   #' maybe(x)
-  #' 
-  #' x <- 
+  #'
+  #' x <-
   #'   c("\\u30cf\\u30c3\\u30ab\\u30b0\\u30b5", "\u30b9\\u30ba\\u30ce\\u30a8\\u30f3\\u30c9\\u30a6")
   #' x <- stringi::stri_unescape_unicode(x)
   #' x
   #' mosiya(x)
-  #' 
+  #'
   #' @export
 search_similar_name <- function(x, len=1, min_dist=4, min_dist_norm=0.2){
   ref <- if(len==6){
@@ -47,13 +47,13 @@ search_similar_name <- function(x, len=1, min_dist=4, min_dist_norm=0.2){
     ref %>%
     dplyr::select(tidyselect::contains("name")) %>%
     .[[1]] %>%
-    purrr::map(editdist, x, len=len) %>%  # editdist: cpp code to spped up
+    purrr::map(editdist, x, len=len) %>%  # editdist: cpp code to speed up
     unlist() %>%
     tibble::tibble(x, ref, dist=.) %>%
     magrittr::set_colnames(c("input", "source", "maybe", "dist"))
   res <-
     if(len==6){  # when wamei (Japanese name), unescape wamei (Japanese name)
-      dplyr::mutate(res, input=stringi::stri_unescape_unicode(input), maybe=stringi::stri_unescape_unicode(maybe)) 
+      dplyr::mutate(res, input=stringi::stri_unescape_unicode(input), maybe=stringi::stri_unescape_unicode(maybe))
     } else {     # when scientific name
       res
     }
@@ -67,7 +67,7 @@ search_similar_name <- function(x, len=1, min_dist=4, min_dist_norm=0.2){
     dplyr::distinct() %>%
     dplyr::mutate(tmp="tmp") %>%
     tidyr::pivot_wider(
-      id_cols=c(input, maybe, dist, dist_norm), names_from=tmp, values_from=source, 
+      id_cols=c(input, maybe, dist, dist_norm), names_from=tmp, values_from=source,
       values_fn = list(source = ~paste(., collapse = "; "))
     )
   if(nrow(res) > 0) dplyr::rename(res, source=tmp) else res
@@ -84,8 +84,8 @@ maybe <- function(x, len=1, min_dist=4, min_dist_norm=0.2){
   #' @describeIn search_similar_name Search similar wamei (Japanese names) from existing data
   #' @export
 mosiya <- function(x, len=6, min_dist=3, min_dist_norm=0.2){
-  x <- 
-    x %>% 
+  x <-
+    x %>%
     stringi::stri_trans_general("halfwidth-fullwidth") %>%
     stringi::stri_escape_unicode()
   maybe(x, len, min_dist, min_dist_norm)
